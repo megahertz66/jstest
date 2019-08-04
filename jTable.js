7f40ca293754ad18f022b55e8a4360dd93bfe57b
Hert Zhao <zhao13836892152@foxmail.com>
2019-08-04 21:59:37 +0800

    1 the stack label step

---
+++ jTable.js
@@ -0,0 +1,12 @@
+// main.html's javascript files
+
+// 处理桌面图标。添加超链接元素，并设置图片把超链接图片按照（参考windows桌面的
+//   形式）右侧排列好，由于图片可能有大有小，需要考虑到将图片的大小设置为相同。
+
+// 找到桌面的高度&宽度
+td_height = window.screen.availHeight;
+td_wight  = window.screen.availWidth;
+td_height -= 30;
+// 添加链接元素
+
+
